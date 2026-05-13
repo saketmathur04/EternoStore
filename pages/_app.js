59c0825023c7de87d35d3e3bf6ebea8e8b20fa24
saketmathur04 <saketmathur2144@gmail.com>
2026-05-14 00:10:14 +0530

    wire up layout in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { Layout } from '../components';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+
+export default MyApp
