d27c7bf5d75ae0bd2c74631e32041fe2f05ed9fa
saketmathur04 <saketmathur2144@gmail.com>
2026-05-16 00:53:59 +0530

    add image gallery with thumbnail selection

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} className="product-detail-image" />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img 
+                key={i}
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
           </div>
         </div>
 
@@ -56,8 +67,6 @@ export const getStaticProps = async ({ params: { slug }}) => {
   
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
-
-  console.log(product);
 
   return {
     props: { products, product }
