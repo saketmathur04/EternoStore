e2da2078ac336da192ef55a9613127cb70853bb9
saketmathur04 <saketmathur2144@gmail.com>
2026-05-17 00:08:38 +0530

    render cart items with product info

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -38,6 +38,38 @@ const Cart = () => {
             </Link>
           </div>
         )}
+
+        <div className="product-container">
+          {cartItems.length >= 1 && cartItems.map((item) => (
+            <div className="product" key={item._id}>
+              <img src={urlFor(item?.image[0])} className="cart-product-image" />
+              <div className="item-desc">
+                <div className="flex top">
+                  <h5>{item.name}</h5>
+                  <h4>${item.price}</h4>
+                </div>
+                <div className="flex bottom">
+                  <div>
+                  <p className="quantity-desc">
+                    <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec') }>
+                    <AiOutlineMinus />
+                    </span>
+                    <span className="num" onClick="">{item.quantity}</span>
+                    <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
+                  </p>
+                  </div>
+                  <button
+                    type="button"
+                    className="remove-item"
+                    onClick={() => onRemove(item)}
+                  >
+                    <TiDeleteOutline />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
