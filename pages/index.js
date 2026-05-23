9624203e4ed2500d5d53a0a4bd7c7937deb54130
saketmathur04 <saketmathur2144@gmail.com>
2026-05-23 17:42:58 +0530

    refine product heading and description text

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
+      <h2>Best Selling Products</h2>
+      <p>Handpicked audio gear loved by thousands of customers worldwide</p>
     </div>
 
     <div className="products-container">
