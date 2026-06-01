b28e8767408657a195056d0921a8a2fdb1c49824
saketmathur04 <saketmathur2144@gmail.com>
2026-06-01 10:20:00 +0530

    add aria-label to cart toggle button for improved accessibility

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <Link href="/">SonicZone</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+      <button type="button" className="cart-icon" aria-label="Open cart" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
       </button>
