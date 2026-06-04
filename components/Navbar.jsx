5b70bac7dda4fd8442dacad86739570d1649b438
saketmathur04 <saketmathur2144@gmail.com>
2026-06-04 10:30:00 +0530

    add aria-label to Navbar logo link for screen readers

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">SonicZone</Link>
+        <Link href="/" aria-label="Home">SonicZone</Link>
       </p>
 
       <button type="button" className="cart-icon" aria-label="Open cart" onClick={() => setShowCart(true)}>
